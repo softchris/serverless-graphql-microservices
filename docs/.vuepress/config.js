1cd59472535e0f99fe8e47afd8e10cffcf3d54b1
Jen Looper <jen.looper@gmail.com>
2019-11-23 13:58:53 -0500

    fixing folder structure for upgrade to 1.2

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    logo: 'bit.png',
+	head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+	title: 'A Workshop all about GraphQL',
+		themeConfig: {
+		search: true,
+		lastUpdated: 'Last Updated',
+		sidebar: [
+            '/workshop/1',
+            '/workshop/2',
+            '/workshop/3',
+            '/workshop/4',
+            '/workshop/5'
+        ]             
+	}
+};
